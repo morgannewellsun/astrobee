7b0f971202c04ecd8101682ce4e9aea89baf46eb
Morgan Sun <morgan.newell.sun@gmail.com>
2023-06-20 10:35:21 -0700

    Getting Aryan's ign gazebo code to run

--- localization/handrail_segmentation/worlds/plugins/build-old/cmake_install.cmake
+++ localization/handrail_segmentation/worlds/plugins/build-old/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/mnsun/ros_ws/astrobee/src/localization/handrail_segmentation/worlds/plugins
+# Install script for directory: /home/anaveen/Documents/nasa_ws/astrobee-detection-pipeline/src/handrail_segmentation/worlds/plugins
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/mnsun/ros_ws/astrobee/src/localization/handrail_segmentation/worlds/plugins/install")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/usr/local/home/mnsun/ros_ws/astrobee/src/localization/handrail_segmentation/worlds/plugins/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/anaveen/Documents/nasa_ws/astrobee-detection-pipeline/src/handrail_segmentation/worlds/plugins/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
