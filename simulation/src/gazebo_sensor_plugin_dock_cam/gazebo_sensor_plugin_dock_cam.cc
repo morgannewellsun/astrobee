1b7cc9295a0e71c6cb3507684bc651e07d498a2f
Morgan Sun <morgan.newell.sun@gmail.com>
2023-07-24 15:40:49 -0700

    [WIP] Temporary changes to camera configurations to allow handrail segmentation to run

--- simulation/src/gazebo_sensor_plugin_dock_cam/gazebo_sensor_plugin_dock_cam.cc
+++ simulation/src/gazebo_sensor_plugin_dock_cam/gazebo_sensor_plugin_dock_cam.cc
@@ -61,8 +61,7 @@ class GazeboSensorPluginDockCam : public FreeFlyerSensorPlugin {
     }
 
     // Check that we have a mono camera
-    if (sensor_->Camera()->ImageFormat() != "L8")
-      ROS_FATAL_STREAM("Camera format must be L8");
+    // if (sensor_->Camera()->ImageFormat() != "L8") ROS_FATAL_STREAM("Camera format must be L8");
 
     // Set image constants
     msg_.is_bigendian = false;
